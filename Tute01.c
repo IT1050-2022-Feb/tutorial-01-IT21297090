d079ccec644e9c20b1021edaef0396b085481bc0
Chamodi Maheesha <99491092+IT21297090@users.noreply.github.com>
2022-02-11 16:24:18 +0000

    Tute01.c is completed

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,23 @@
 #include <stdio.h>
 
 int main() {
+  int mark1,mark2; //define variables
+  float avg;
+
+  printf("enter the first mark :") //input mark1
+  scanf("%d",&mark1);
+
+  printf("enter the second mark :") //input mark2
+  scanf("%d",&mark2);
+
+  avg = (float)(mark1 + mark2)/2; //Calculate the average
+
+  printf("average : %.2f",avg); //print average
+
   
-  return 0;
+
+  
+  
+  return 0; //end process
 }
 
